7d88f466a564433bbf029d763bdd0594338e8ecd
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-08 15:50:14 +0430

    Create game class

---
+++ classes/game/game.rb
@@ -0,0 +1,15 @@
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer, last_played_at, publish_date)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  private
+
+  def can_be_archived?()
+    true
+  end
+end
