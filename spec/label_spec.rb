89db28d97fef98941c4e6a34b669fc1ecb24b243
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-08-08 19:57:13 +0300

    Add tests for book and Label classes

---
+++ spec/label_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../classes/label'
+require_relative '../classes/book'
+describe 'label' do
+  context 'Testing label class' do
+    book = Book.new('John', 'good', Date.parse('2023-08-08'))
+    label = Label.new('Book-label', 'Red')
+
+    it 'has the label title' do
+      expect(label.title).to eq 'Book-label'
+    end
+
+    it 'has the label title' do
+      expect(label.color).to eq 'Red'
+    end
+
+    it 'add book item object to label items array' do
+      label.add_item(book)
+      expect(label.items[0]).to eq book
+    end
+  end
+end
