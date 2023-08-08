a89ece30b58618791b750a77e5a8f870b8609dfc
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-08 10:23:28 +0430

    add

--- classes/item.rb
+++ classes/item.rb
@@ -1,6 +1,6 @@
 class Item
-  attr_accessor :publish_date
-  attr_reader :id, :archived, :genre , :label, :author, :source
+  attr_accessor :publish_date, :archived
+  attr_reader :id, :genre, :label, :author, :source  
 
   def initialize(publish_date, archived: false)
     @id = Random.rand(1...1000)
@@ -25,5 +25,16 @@ class Item
 
   def source=(source)
     @source = source
+  end
+
+  def move_to_archive()
+    @archived = true if can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?()
+    return true if @publish_date > 10
+    return false
   end
 end
