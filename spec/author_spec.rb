ba0a66e96530240475e7236b1bc3370b683b9724
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-08 20:09:50 +0430

    Create unit_test for author class

---
+++ spec/author_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../classes/game/author'
+require_relative '../classes/item'
+
+describe Author do
+  context 'I create the author object and test it' do
+    let(:author) { Author.new('Alishah', 'Safdari') }
+    let(:item) { Item.new(12) }
+    it 'Author first_name must be Alishah' do
+      expect(author.first_name).to eq('Alishah')
+    end
+    it 'Author last name must be Safdari' do
+      expect(author.last_name).to eq('Safdari')
+    end
+    it 'sets the item author to the current author' do
+      author.add_item(item)
+      expect(item.author).to eq(author)
+    end
+    it 'does not add the same item twice' do
+      author.add_item(item)
+      expect do
+        author.add_item(item)
+      end.not_to(change { author.items.length })
+    end
+    it 'adds an item to the author' do
+      expect do
+        author.add_item(item)
+      end.to change { author.items.length }.by(1)
+    end
+    it 'adds an item to the author' do
+      author.add_item(item)
+      expect(author.items).to include(item)
+    end
+  end
+end
