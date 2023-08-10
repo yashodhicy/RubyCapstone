e075e0f194399d55cd03442508ef8e599fe3b579
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-10 13:54:10 +0430

    Add game and author schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE my_catalog;
+
 CREATE TABLE label (
     id  INT GENERATED ALWAYS AS IDENTITY,
     title VARCHAR(100),
@@ -10,7 +12,9 @@ CREATE TABLE item (
   publish_date DATE NOT NULL,
   archived BOOLEAN DEFAULT false,
   label_id INT,
+  author_id INT,
   FOREIGN KEY (label_id) REFERENCES labels(id),
+  FOREIGN KEY (author_id) REFERENCES author(id)
 );
 
 CREATE TABLE book (
@@ -18,3 +22,17 @@ CREATE TABLE book (
   publisher VARCHAR(255) NOT NULL,
   cover_state VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE author(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(40),
+  last_name VARCHAR(40),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE games(
+    id INTGENERATED ALWAYS AS IDENTITY,
+    last_palyed DATE,
+    multiplayer BOOLEAN,
+    CONSTRAINT item_fk FOREIGN KEY (id) REFERENCES items (id)
+)
